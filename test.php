9ec10fe559158db2dcdd79b87b6823a1b7105247
Sergio Enrique Vargas <sergioenrique@me.com>
2016-07-27 17:22:16 -0500

    Comenzando a crear la calculadora

--- test.php
+++ test.php
@@ -5,43 +5,124 @@ require __DIR__ . '/vendor/autoload.php';
 use Valerio\FormulaToken;
 use Valerio\FormulaParser;
 
-/*
-	Copyright (c) 2007 E. W. Bachtal, Inc.
-	Ported to PHP by Maarten Balliauw (http://www.balliauw.be/maarten)
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-	and associated documentation files (the "Software"), to deal in the Software without restriction, 
-	including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-	and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
-	subject to the following conditions:
-	
-	  The above copyright notice and this permission notice shall be included in all copies or substantial 
-	  portions of the Software.
-	
-	The software is provided "as is", without warranty of any kind, express or implied, including but not 
-	limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In 
-	no event shall the authors or copyright holders be liable for any claim, damages or other liability, 
-	whether in an action of contract, tort or otherwise, arising from, out of or in connection with the 
-	software or the use or other dealings in the software. 
-	
-	http://ewbi.blogs.com/develops/2007/03/excel_formula_p.html
-	http://ewbi.blogs.com/develops/2004/12/excel_formula_p.html
+/**
+*   Fórmula de excel parcial, puede ser "IF", etc.
 */
-
-// Parse
-$test = new FormulaParser('=IF(TRUE,10,5)');
-
-// Print
-$indent 	= 1;
-$token 		= null;
-echo "Formula: " . $test->getFormula() . "<br>";
-
-for ($i = 0; $i < $test->getTokenCount(); $i++) {
-	$token = $test->getToken($i);
-	
-	if ($token->getTokenSubType() == FormulaToken::TOKEN_SUBTYPE_STOP ) $indent--;
-	
-	printf("%-3s&nbsp&nbsp&nbsp&nbsp%-20s&nbsp&nbsp&nbsp&nbsp%-10s&nbsp&nbsp&nbsp&nbsp%-10s&nbsp&nbsp&nbsp&nbsp%-30s<br>", $i, $token->getTokenType(), $token->getTokenSubType(), $token->getValue(), str_repeat("| ", $indent) . $token->getValue());
-	
-	if ($token->getTokenSubType() == FormulaToken::TOKEN_SUBTYPE_START ) $indent++;
+class ExcelFunction
+{
+	public $name;
+	public $content;
 }
+
+/**
+*   Función IF
+*/
+class IfFunction extends ExcelFunction
+{
+	
+}
+
+/**
+*   Arbol de fórmula completo
+*/
+class ParsedTree
+{
+	public $tree;
+
+
+}
+
+/**
+*  Pila del árbol para crear el ParsedTree con la calculadora
+*/
+class TreeStack
+{
+	private $pila;
+
+	function __construct()
+	{
+		$this->pila = array();
+	}
+
+	public function show()
+	{
+		return json_encode($this->pila);
+	}
+
+	public function add($value)
+	{
+		$this->pila[] = $value;
+	}
+}
+
+/**
+*   Cómputo de Fórmulas
+*/
+class Compute
+{
+	private $stack;
+
+	private $result;
+
+	function __construct($formula)
+	{
+		$this->stack = new TreeStack();
+		$formulaParser = new FormulaParser($formula);
+		$this->TreeCreator($formulaParser);
+	}
+
+	public function getResult()
+	{
+		return $this->result;
+	}
+
+	public function TreeCreator(Valerio\FormulaParser $parsedTree)
+	{
+		$indent = 1;
+		$token = null;
+
+		// $formula = $parsedTree->getFormula();
+
+		// Crear árbol
+		for ($i = 0; $i < $parsedTree->getTokenCount(); $i++) {
+			$token = $parsedTree->getToken($i);
+			
+			if ($token->getTokenSubType() == FormulaToken::TOKEN_SUBTYPE_STOP ) $indent--;
+
+			$this->analyzeType($token->getTokenType());
+
+			// printf("%-3s&nbsp&nbsp&nbsp&nbsp%-20s&nbsp&nbsp&nbsp&nbsp%-10s&nbsp&nbsp&nbsp&nbsp%-10s&nbsp&nbsp&nbsp&nbsp%-30s<br>", $i, $token->getTokenType(), $token->getTokenSubType(), $token->getValue(), str_repeat("| ", $indent) . $token->getValue());
+			
+			if ($token->getTokenSubType() == FormulaToken::TOKEN_SUBTYPE_START ) $indent++;
+		}
+
+		$this->result = $this->stack->show();
+	}
+
+	private function analyzeType($type)
+	{
+		switch ($type) {
+			case 'Function':
+				$this->stack->add($type);
+				break;
+			
+			default:
+				$this->stack->add("nada");
+				break;
+		}
+	}
+
+}
+
+
+// Trabajo completo
+$computo = new Compute('=IF(TRUE,10,5)');
+
+echo $computo->getResult();
+
+
+
+
+
+
+
