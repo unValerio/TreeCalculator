3c0368fd63ed2c331d4d4f50b4cdfa37311997ed
Sergio Enrique Vargas <sergioenrique@me.com>
2016-07-28 01:30:58 -0500

    integrando operadores de comparación

--- test.php
+++ test.php
@@ -6,7 +6,8 @@ use Valerio\Compute;
 
 
 
-$formula = '=IF(TRUE,IF(FALSE,69,IF(FALSE,599,987)),IF(FALSE,222,323))';
+//$formula = '=IF("SI"="SI",9876,69)';
+$formula = '=IF(FALSE,90,IF("ESTE"="ESTE",67,IF(TRUE,12,43)))';
 
 $computo = new Compute($formula);
 
--- valerio/Operators.php
+++ valerio/Operators.php
@@ -10,14 +10,44 @@ class Operators
 	public function startFunction($stack)
 	{
 		// Obtener el tipo de función
-		$function = $stack[sizeof($stack)-1]['value'];
+		// $function = $stack[sizeof($stack)-1]['value'];
+		$function = array_pop($stack)['value'];
 
 		// Qué hacer si es IF
 		if ($function === 'IF') {
 
 			$result = 0;
 
-			$logical = $stack[sizeof($stack)-2]['value'];
+			$newStack = array();
+			$row = array_pop($stack);
+			while ($row['subtype'] != 'Union') {
+				$newStack[] = $row;
+				$row = array_pop($stack);
+			}
+
+			// Si hay una condición tipo "SI" = "SI"
+			if (sizeof($newStack) === 3) {
+				$logical = 'FALSE';
+				switch ($newStack[1]['value']) {
+					case '=': // igual que
+						if ($newStack[0]['value'] === $newStack[2]['value']) {
+							$logical = 'TRUE';
+						}
+						break;
+					case '<>': // diferente de
+						if ($newStack[0]['value'] != $newStack[2]['value']) {
+							$logical = 'TRUE';
+						}
+						break;
+					default:
+						break;
+				}
+				$stack[] = ''; //Para que el arreglo sea del mismo tamaño que la condición tipo TRUE
+			}
+			else{
+				// $logical = $stack[sizeof($stack)-2]['value'];
+				$logical = $newStack[0]['value'];
+			}
 
 			if ($logical === 'TRUE') {
 				$result = $stack[3]['value'];
