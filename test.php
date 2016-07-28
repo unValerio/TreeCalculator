533eb6730b36a6219a2a8f8108b29ae465e6ce21
Sergio Enrique Vargas <sergioenrique@me.com>
2016-07-27 20:52:58 -0500

    primer cálculo completado

--- test.php
+++ test.php
@@ -6,37 +6,56 @@ use Valerio\FormulaToken;
 use Valerio\FormulaParser;
 
 /**
-*   Fórmula de excel parcial, puede ser "IF", etc.
+*   Operadores
 */
-class ExcelFunction
+class Operators
 {
-	public $name;
-	public $content;
-}
+	// Ahorita sólo funciona para IF(TRUE,5,10)
+	public function startFunction($stack)
+	{
+		// Obtener el tipo de función
+		$function = array_pop($stack)['value'];
 
-/**
-*   Función IF
-*/
-class IfFunction extends ExcelFunction
-{
-	
-}
+		// Qué hacer si es IF
+		if ($function === 'IF') {
 
-/**
-*   Arbol de fórmula completo
-*/
-class ParsedTree
-{
-	public $tree;
+			$result = 0;
 
+			$logical = array_pop($stack)['value'];
 
+			if ($logical === 'TRUE') {
+				//echo '<p style="color:red">'.$stack[3]['value'].'</p>';
+				$result = $stack[3]['value'];
+			}
+			elseif($logical === 'FALSE'){
+				//echo '<p style="color:red">'.$stack[1]['value'].'</p>';
+				$result = $stack[1]['value'];
+			}
+
+			return array(
+				'index' => $stack[sizeof($stack)-1]['index'].'-'.$stack[0]['index'],
+				'type' => 'Operand',
+				'subtype' => 'Number',
+				'value' => $result,
+				'deep' => $stack[0]['deep']
+			);
+		}
+
+		return array(
+			'index' => $stack[0]['index'].'-'.$stack[sizeof($stack)-1]['index'],
+			'type' => 'Operand',
+			'subtype' => 'Number',
+			'value' => 0,
+			'deep' => $stack[0]['deep']
+		);
+	}
 }
 
 /**
 *  Pila del árbol para crear el ParsedTree con la calculadora
 *  Esta es una pila inteligente que analiza el valor que tiene el último elemento y si es stop y coincide con su deep, entonces esa parte la analiza (convierte a clase función)
 */
-class TreeStack
+class FormulaStack
 {
 	private $stack;
 
@@ -55,7 +74,8 @@ class TreeStack
 				$newStack[] = $currentRow;
 			} while ($currentRow['subtype'] != 'Start');
 
-			$this->stack[] = array_reverse($newStack);
+			$this->stack[] = Operators::startFunction($newStack);
+			// $this->stack[] = $newStack;
 		}
 		else{
 			$this->stack[] = $row;
@@ -64,7 +84,8 @@ class TreeStack
 
 	public function show()
 	{
-		return $this->stack;
+		return $this->stack[0]['value'];
+		// return $this->stack;
 	}
 
 }
@@ -80,7 +101,7 @@ class Compute
 
 	function __construct($formula)
 	{
-		$this->stack = new TreeStack();
+		$this->stack = new FormulaStack();
 		$formulaParser = new FormulaParser($formula);
 		$this->TreeCreator($formulaParser);
 	}
@@ -112,7 +133,7 @@ class Compute
 			);
 			$this->stack->push($row);
 
-			//printf("%-3s&nbsp&nbsp&nbsp&nbsp%-20s&nbsp&nbsp&nbsp&nbsp%-10s&nbsp&nbsp&nbsp&nbsp%-10s&nbsp&nbsp&nbsp&nbsp%-30s<br>", $i, $token->getTokenType(), $token->getTokenSubType(), $token->getValue(), str_repeat("| ", $indent) . $token->getValue());
+			// printf("%-3s&nbsp&nbsp&nbsp&nbsp%-20s&nbsp&nbsp&nbsp&nbsp%-10s&nbsp&nbsp&nbsp&nbsp%-10s&nbsp&nbsp&nbsp&nbsp%-30s<br>", $i, $token->getTokenType(), $token->getTokenSubType(), $token->getValue(), str_repeat("| ", $indent) . $token->getValue());
 			
 			if ($token->getTokenSubType() == FormulaToken::TOKEN_SUBTYPE_START ) $indent++;
 		}
@@ -124,7 +145,7 @@ class Compute
 
 
 // Trabajo completo
-$formula = '=IF(TRUE,IF(FALSE,3,2),5)';
+$formula = '=IF(FALSE,54,IF(TRUE,222,323))';
 
 $computo = new Compute($formula);
 
